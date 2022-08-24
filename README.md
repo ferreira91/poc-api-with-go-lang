ac918ebfcbc750bc73977bca460c61df9f5fdc83
Caina Ferreira da Silva <caina.silva@zup.com.br>
2022-08-23 21:51:06 -0300

    add infrastructure

---
+++ README.md
@@ -0,0 +1,8 @@
+# poc-api-with-go-lang
+### Requirements
+* [Docker](https://www.docker.com/)
+* [Make](https://www.gnu.org/software/make/)
+
+### How to prepare enviroment 
+To create database and import data, run into infrastructure directory:
+```$ make up```
---
+++ infrastructure/db/postgres/scripts/1_init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE market (
+   id serial PRIMARY KEY,
+   longitude VARCHAR ( 10 ) NOT NULL,
+   latitude VARCHAR ( 10 ) NOT NULL,
+   census_sector VARCHAR ( 15 ) NOT NULL,
+   weighting_area VARCHAR ( 13 ) NOT NULL,
+   township_code VARCHAR ( 9 ) NOT NULL,
+   township VARCHAR ( 18 ) NOT NULL,
+   subprefecture_code VARCHAR ( 2 ) NOT NULL,
+   subprefecture VARCHAR ( 25 ) NOT NULL,
+   region_5 VARCHAR ( 6 ) NOT NULL,
+   region_8 VARCHAR ( 7 ) NOT NULL,
+   name VARCHAR ( 30 ) NOT NULL,
+   registry VARCHAR ( 6 ) UNIQUE NOT NULL,
+   public_place VARCHAR ( 34 ) NOT NULL,
+   number VARCHAR ( 15 ),
+   district VARCHAR ( 20 ),
+   reference VARCHAR ( 30 )
+);
