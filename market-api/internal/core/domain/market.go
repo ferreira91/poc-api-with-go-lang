f3eeed54deb83e3c0114a48647c1e91faeb3809c
Caina Ferreira da Silva <caina.silva@zup.com.br>
2022-08-25 22:31:01 -0300

    add domain

---
+++ market-api/internal/core/domain/market.go
@@ -0,0 +1,37 @@
+package domain
+
+import (
+	"github.com/asaskevich/govalidator"
+)
+
+type IMarket interface {
+	IsValid() (bool, error)
+}
+
+type Market struct {
+	ID                string `valid:"optional"`
+	Longitude         string `valid:"required"`
+	Latitude          string `valid:"required"`
+	CensusSector      string `valid:"required"`
+	WeightingArea     string `valid:"required"`
+	Township		  string `valid:"required"`
+	TownshipCode      string `valid:"required"`
+	SubPrefectureCode string `valid:"required"`
+	SubPrefecture     string `valid:"required"`
+	Region5           string `valid:"required"`
+	Region8           string `valid:"required"`
+	Name              string `valid:"required"`
+	Registry          string `valid:"required"`
+	Street            string `valid:"required"`
+	Number            string `valid:"optional"`
+	District          string `json:"required"`
+	Reference         string `valid:"optional"`
+}
+
+func (m *Market) IsValid() (bool, error) {
+	_, err := govalidator.ValidateStruct(m)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
---
+++ market-api/internal/core/domain/market_test.go
@@ -0,0 +1,66 @@
+package domain
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMarket_IsValid(t *testing.T) {
+	market := Market{}
+	market.ID = "1234567"
+	market.Longitude = "-1111111"
+	market.Latitude = "-9999999"
+	market.CensusSector = "CensusSector"
+	market.WeightingArea = "WeightingArea"
+	market.TownshipCode = "123"
+	market.Township = "Township"
+	market.SubPrefectureCode = "00"
+	market.SubPrefecture = "SubPrefecture"
+	market.Region5 = "Region5"
+	market.Region8 = "Region8"
+	market.Name = "Name"
+	market.Registry = "123456"
+	market.Street = "Street"
+	market.Number = "123"
+	market.District = "District"
+	market.Reference = "Reference"
+	_, err := market.IsValid()
+	require.Nil(t, err)
+
+	market.ID = ""
+	market.Number = ""
+	market.Reference = ""
+	_, err = market.IsValid()
+	require.Nil(t, err)
+
+	market.Longitude = ""
+	market.Latitude = ""
+	market.CensusSector = ""
+	market.WeightingArea = ""
+	market.TownshipCode = ""
+	market.Township = ""
+	market.SubPrefectureCode = ""
+	market.SubPrefecture = ""
+	market.Region5 = ""
+	market.Region8 = ""
+	market.Name = ""
+	market.Registry = ""
+	market.Street = ""
+	market.District = ""
+	_, err = market.IsValid()
+	require.Equal(t,
+		"CensusSector: non zero value required;"+
+			"Latitude: non zero value required;"+
+			"Longitude: non zero value required;"+
+			"Name: non zero value required;"+
+			"Region5: non zero value required;"+
+			"Region8: non zero value required;"+
+			"Registry: non zero value required;"+
+			"Street: non zero value required;"+
+			"SubPrefecture: non zero value required;"+
+			"SubPrefectureCode: non zero value required;"+
+			"Township: non zero value required;"+
+			"TownshipCode: non zero value required;"+
+			"WeightingArea: non zero value required",
+		err.Error())
+}
