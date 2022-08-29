28f794cc7528e9a3f7a08ed25c587bbc9128c5d2
Caina Ferreira da Silva <caina.silva@zup.com.br>
2022-08-29 20:39:27 -0300

    create interfaces

--- market-api/internal/core/domain/market.go
+++ market-api/internal/core/domain/market.go
@@ -8,13 +8,50 @@ type IMarket interface {
 	IsValid() (bool, error)
 }
 
+type IMarketService interface {
+	ICreateMarketService
+	IFindMarketService
+	IUpdateMarketService
+	IDeleteMarketService
+}
+
+type ICreateMarketService interface {
+	Create(market IMarket) (IMarket, error)
+}
+
+type IFindMarketService interface {
+	GetByID(id string) (IMarket, error)
+	GetAll() ([]IMarket, error)
+	Get(township string, region5 string, name string, district string) ([]IMarket, error)
+}
+
+type IUpdateMarketService interface {
+	Update(id string, market IMarket) (IMarket, error)
+}
+
+type IDeleteMarketService interface {
+	DeleteByRegistry(registry string) error
+}
+
+type IMarketReaderPersistence interface {
+	FindByID(id string) (IMarket, error)
+	FindAll() ([]IMarket, error)
+	Find(query map[string]string) ([]IMarket, error)
+}
+
+type IMarketWriterPersistence interface {
+	Save(market IMarket) (IMarket, error)
+	Update(id string, market IMarket) (IMarket, error)
+	DeleteByRegistry(registry string) (int64, error)
+}
+
 type Market struct {
 	ID                string
 	Longitude         string `valid:"required"`
 	Latitude          string `valid:"required"`
 	CensusSector      string `valid:"required"`
 	WeightingArea     string `valid:"required"`
-	Township		  string `valid:"required"`
+	Township          string `valid:"required"`
 	TownshipCode      string `valid:"required"`
 	SubPrefectureCode string `valid:"required"`
 	SubPrefecture     string `valid:"required"`
@@ -24,7 +61,7 @@ type Market struct {
 	Registry          string `valid:"required"`
 	Street            string `valid:"required"`
 	Number            string
-	District          string `json:"required"`
+	District          string `valid:"required"`
 	Reference         string
 }
 
@@ -36,6 +73,6 @@ func (m *Market) IsValid() (bool, error) {
 	return true, nil
 }
 
-func NewMarket() *Market  {
+func NewMarket() *Market {
 	return &Market{}
 }
