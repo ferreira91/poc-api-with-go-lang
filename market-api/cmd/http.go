9aea7ef4e67438013aa74c2f4a8cf605ab04eb82
Caina Ferreira da Silva <caina.silva@zup.com.br>
2022-09-06 23:59:25 -0300

    fix config docker

--- market-api/cmd/http.go
+++ market-api/cmd/http.go
@@ -7,43 +7,32 @@ package cmd
 import (
 	"database/sql"
 	"github.com/spf13/cobra"
-	"log"
 	"market-api/internal/api/web"
 	"market-api/internal/core/service"
 	"market-api/internal/db/postgres"
 	postgres2 "market-api/test/postgres"
-	"os"
 )
 
 // httpCmd represents the http command
 var httpCmd = &cobra.Command{
 	Use:   "http",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "API Market",
+	Long: "API Market ",
 	Run: func(cmd *cobra.Command, args []string) {
-		f, err := os.OpenFile("testlogfile", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("error opening file: %v", err)
-		}
-		defer f.Close()
-
-		log.SetOutput(f)
-		log.Println("This is a test log entry")
-
-		var db *sql.DB
+		var database *sql.DB
 		if environment == "LOCAL" {
-			_, db, _ = postgres2.SetUp()
+			_, db, _ := postgres2.SetUp()
+			database = db
 		} else {
 			p := postgres.Init()
-			db = p.Start()
+			db, err := p.Start()
+			if err != nil {
+				panic(err.Error())
+			}
+			database = db
 		}
 
-		var repository = postgres.NewMarketDb(db)
+		var repository = postgres.NewMarketDb(database)
 		var marketService = service.MarketService{Persistence: repository}
 
 		server := web.NewServer()
--- market-api/internal/db/postgres/db.go
+++ market-api/internal/db/postgres/db.go
@@ -11,7 +11,7 @@ import (
 type Config struct {
 	DriverName string
 	Host       string
-	Port       string
+	Port       int
 	User       string
 	Password   string
 	DbName     string
@@ -22,7 +22,7 @@ func Init() Config {
 	return Config{
 		DriverName: viper.GetString("DB_DRIVER_NAME"),
 		Host:       viper.GetString("DB_HOST"),
-		Port:       viper.GetString("DB_PORT"),
+		Port:       viper.GetInt("DB_PORT"),
 		User:       viper.GetString("DB_USER"),
 		Password:   viper.GetString("DB_PASSWORD"),
 		DbName:     viper.GetString("DB_NAME"),
@@ -30,52 +30,19 @@ func Init() Config {
 	}
 }
 
-func (c *Config) Start() *sql.DB {
+func (c *Config) Start() (*sql.DB, error) {
 	dataSourceName := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		c.Host, 5432, "postgres", "postgres", "market")
+		c.Host, c.Port, c.User, c.Password, c.DbName)
 	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
-		log.Fatal(err)
-		panic(err.Error())
+		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
-		panic(err.Error())
+		return nil, err
 	}
+
 	log.Println("Database connection established")
-	return db
-
-	//connStr := fmt.Sprintf(
-	//	"host=%s port=%d user=%s password=%s  dbname=%s sslmode=disable",
-	//	"db", 5431, "postgres", "postgres", "market",
-	//)
-	//db2, err := sql.Open("postgres", connStr)
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//	print(db2.Ping())
-	//}
-	//println(db2.Ping())
-	//return db2
-
-	//var dataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-	//	c.Host, c.Port, c.User, c.Password, c.DbName, c.SslMode)
-	//println(dataSourceName)
-	//log.Printf("String connection: %s", dataSourceName)
-	//
-	//db, err := sql.Open("postgers", dataSourceName)
-	//if err != nil {
-	//	println(err)
-	//	log.Fatal(err)
-	//	return nil
-	//}
-	//err = db.Ping()
-	//if err != nil {
-	//	println(err)
-	//	log.Fatal(err)
-	//	return nil
-	//}
-	//log.Println("Database connection established")
-	//return db
+	return db, nil
 }
